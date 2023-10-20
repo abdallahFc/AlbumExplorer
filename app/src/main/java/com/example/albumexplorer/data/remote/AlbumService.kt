59e6734e0f860dcb163d41f828a7741f80202681
Abdallah Mohamed <abdallah.mohamed2274@gmail.com>
2023-10-20 08:50:16 +0300

    add service class

--- app/src/main/java/com/example/albumexplorer/data/remote/AlbumService.kt
+++ app/src/main/java/com/example/albumexplorer/data/remote/AlbumService.kt
@@ -1,9 +1,10 @@
 package com.example.albumexplorer.data.remote
 
 import com.example.albumexplorer.data.remote.model.UsersDto
+import retrofit2.Response
 import retrofit2.http.GET
 
 interface AlbumService {
     @GET("/users")
-    suspend fun getUsers(): UsersDto
+    suspend fun getUsers(): Response<List<UsersDto>>
 }
