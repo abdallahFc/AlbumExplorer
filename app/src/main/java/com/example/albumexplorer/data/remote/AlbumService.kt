0a32b5817c7ef1fed28affcea9ca95edf1350fca
Abdallah Mohamed <abdallah.mohamed2274@gmail.com>
2023-10-20 08:53:20 +0300

    add all end point

--- app/src/main/java/com/example/albumexplorer/data/remote/AlbumService.kt
+++ app/src/main/java/com/example/albumexplorer/data/remote/AlbumService.kt
@@ -1,10 +1,18 @@
 package com.example.albumexplorer.data.remote
 
+import com.example.albumexplorer.data.remote.model.AlbumsItemDto
+import com.example.albumexplorer.data.remote.model.PhotosItemDto
 import com.example.albumexplorer.data.remote.model.UsersDto
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface AlbumService {
     @GET("/users")
     suspend fun getUsers(): Response<List<UsersDto>>
+    @GET("/albums")
+    suspend fun getAlbums(@Query("userId") userId: Int):Response<List<AlbumsItemDto>>
+
+    @GET("/photos")
+    suspend fun getPhotos(@Query("albumId") albumId: Int): Response<List<PhotosItemDto>>
 }
